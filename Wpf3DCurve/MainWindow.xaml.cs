e91b659dda4d543ad29f33e0bfbded1d458fea69
Jay Shi <JayShi@eurofins.com>
2020-10-23 17:05:15 +0800

    Initial

--- Wpf3DCurve/MainWindow.xaml.cs
+++ Wpf3DCurve/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wpf3DCurve
 {
@@ -20,9 +8,51 @@ namespace Wpf3DCurve
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double _startPositionX = 0d; 
+        private double _startPositionY = 0d; 
+
+        private bool isLeftButtonDown = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var currentPosition = e.GetPosition(this);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Roate3DOjbect.Angle = currentPosition.X - _startPositionX;
+            }
+            else
+            {
+                _startPositionX = currentPosition.X;
+                _startPositionY = currentPosition.Y;
+            }
+        }
+
+        private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scaleRatio = 0.1d;
+            if (e.Delta < 0)
+            {
+                scaleRatio = -scaleRatio;
+            }
+
+            Scale3DObject.ScaleX = GetScale(Scale3DObject.ScaleX, scaleRatio);
+            Scale3DObject.ScaleY = GetScale(Scale3DObject.ScaleY, scaleRatio);
+            Scale3DObject.ScaleZ = GetScale(Scale3DObject.ScaleZ, scaleRatio);
+        }
+
+        private double GetScale(double orginalSize, double addSize)
+        {
+            if (orginalSize > 0)
+            {
+                return orginalSize + addSize;
+            }
+
+            return orginalSize;
         }
     }
 }
